2d0aa85ba88d789fcf69251c1b41f8a8e537e66f
0x6775737461 <fabiogustavo@tuta.io>
2022-04-19 16:59:45 -0400

    changing the characters to integer format

--- credit.c
+++ credit.c
@@ -14,11 +14,18 @@ int main(){
     printf("%s\n",card_number);
 
     // the last digit of the check_sum must be 0,
-    for(j=1, i=0; i<16; i += 2, j += 2){
-        check_i += (card_number[i] * 2);
-        check_p += (card_number[j]);
+    for(j = 1, i = 0; i < 16; i += 2, j += 2){
+
+        int temp_1 = card_number[i] - '0';
+        check_i += (temp_1 * 2);
+
+        int temp_2 = card_number[j] - '0';
+        check_p += temp_2;
+
+        printf("%d - %d\n", temp_1, temp_2);
     }
 
     check_sum = check_i + check_p;
+
     printf("%d %d %d\n", check_i, check_p, check_sum);
 }
